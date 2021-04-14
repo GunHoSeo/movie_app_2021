01925fa26e2321fd918fb1eead04b4fde4065261
서건호 <gunho@seogeonhoui-MacBookAir.local>
2021-04-14 23:52:04 +0900

    20210414 react commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 
-function Potato(){
-  return <h1> i hate potato</h1>;
+function Food({name,picture}){
+  return (<div>
+    <h1> i like {name}</h1>
+    <img src = {picture}></img>
+    </div>);
 
 }
+const foodLike = [
+{
+  name : 'kimchi',
+  image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.QKNL3H-b8pGMgfg77Ig8BAHaE8%26pid%3DApi&f=1'
+}
 
+]
 
 function App() {
   return (
-  <div> <h1>react pratice</h1> <Potato/> </div>
+  <div> {foodLike.map(dish =>(<Food name={dish.name} picture ={dish.image}/>))}
+  </div>
     )
 }
 
